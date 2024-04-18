641475c2c3e0bae42214a5f77a0323c0f7f85498
dohyung01 <154952683+dohyung01@users.noreply.github.com>
2024-04-18 23:22:44 +0900

    new1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from 'react';
+
 import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { SyncLoader } from "react-spinners"
-import { CustomSyncLoaderPage } from '../src/components/styledComponents'
+
 import Navbar from '../src/components/Navbar';
 import Footer from '../src/components/Footer';
 
